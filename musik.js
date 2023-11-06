cd13db9d55e2e48f4c999f24b31bb20df8a79072
rifanseftiawan <rseftiawan909@gmail.com>
2023-11-06 10:08:05 -0800

    first commit

---
+++ musik.js
@@ -0,0 +1,5 @@
+// script.js
+const audio = document.getElementById('myAudio');
+
+// Untuk mengubah volume audio (opsional)
+audio.volume = 0.5; // Ganti dengan nilai volume yang diinginkan antara 0 dan 1
---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById('tombol').addEventListener('click', function() {
+    document.getElementById('hasil').innerText = 'Tombol telah diklik!';
+});
+document.getElementById('tombol').addEventListener('click', function() {
+    // Mengarahkan pengguna ke halaman baru
+    window.location.href = 'musik.html';
+});
